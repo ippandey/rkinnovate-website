ad84b48b823d09650be9fe37b2ce9f9793cc94b8
amitsuthar803 <amitsuthar803@gmail.com>
2024-10-02 00:06:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center bg-teal-400">
+      <h1>Heading</h1>
+    </div>
+  );
+}
+
+export default App;
