2377be33ccfab8aac957e61f68d75d66ca4db608
amitsuthar803 <amitsuthar803@gmail.com>
2024-10-02 00:36:59 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="flex items-center justify-center bg-teal-400">
-      <h1>Heading</h1>
+      <h1>Welcome to Rk Innovate.</h1>
     </div>
   );
 }
