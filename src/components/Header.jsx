69e5228b9ad2565376959761eb6d20f3da3781ae
Priyanshu Pandey <97188634+ippandey@users.noreply.github.com>
2024-10-09 23:33:53 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,39 @@
 import logo from "../../public/logo.png";
 function Header() {
   return (
-    <div className=" h-[85px] sticky  z-50 top-0 flex shadow-sm items-center justify-between px-8 bg-white border-b-2 ">
-      <div>
-        <a href="/">
-          <img src={logo} alt="logo"  className=" w-[15%]" />
+    <section className="bg-white shadow-sm border-b-2">
+      <div className="h-[85px] sticky z-50 top-0 flex items-center justify-between px-4 md:px-8">
+        <div>
+          <a href="/">
+            <img src={logo} alt="logo" className="w-[30%] md:w-[15%] h-auto" />
+          </a>
+        </div>
+
+        {/* Uncomment to include navigation */}
+        {/* <nav>
+          <ul className="hidden md:flex gap-5 font-semibold">
+            <li>
+              <a href="#">Why Us</a>
+            </li>
+            <li>
+              <a href="#">What We Do</a>
+            </li>
+            <li>
+              <a href="#">Who We Are</a>
+            </li>
+            <li>
+              <a href="#">Work With Us</a>
+            </li>
+          </ul>
+        </nav> */}
+
+        <a href="#contact-us">
+          <button className="border-2 px-4 py-2 md:px-10 md:py-2 rounded-full text-primary font-bold hover:border-primary transition-all duration-500">
+            Contact Us
+          </button>
         </a>
       </div>
-
-      {/* <nav>
-        <ul className="flex gap-5 font-semibold">
-          <li>
-            <a href="#">Why Us</a>
-          </li>
-          <li>
-            <a href="#">What We Do</a>
-          </li>
-          <li>
-            <a href="#">Who We Are</a>
-          </li>
-          <li>
-            <a href="#">Work With Us</a>
-          </li>
-        </ul>
-      </nav> */}
-
-      <a href="#contact-us">
-        <button className="border-2 px-10 py-2 rounded-full text-primary font-bold hover:border-primary transition-all duration-500">
-          Contact Us
-        </button>
-      </a>
-    </div>
+    </section>
   );
 }
 
